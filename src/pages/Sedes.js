0804bf03d7aa3bad91eab2ba085326b2f17a8991
Rulman Ferro <rulman26@gmail.com>
2022-11-02 13:02:09 -0500

    sync hook

--- src/pages/Sedes.js
+++ src/pages/Sedes.js
@@ -1,5 +1,7 @@
+import { useState } from "react"
 
 const Sedes = () => {
+    const [sedes, setSedes] = useState([{ nombre: "Chiquibambilla" }, { nombre: "Abancay" }])
     return (
         <div>
             <div className="container-fluid p-5 bg-primary text-white text-center">
@@ -9,21 +11,18 @@ const Sedes = () => {
 
             <div className="container mt-5">
                 <div className="row">
-                    <div className="col-sm-4">
-                        <h3>Column 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div className="col-sm-4">
-                        <h3>Column 2</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                    <div className="col-sm-4">
-                        <h3>Column 3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
+                    {
+                        sedes.map(sede => {
+                            return (
+                            <div className="col-sm-4">
+                                <h3>{sede.nombre}</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                            </div>
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         </div>
